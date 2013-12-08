dac83344d1d546b5d5289846b4dbf15f8d69cfd9
Lee Miller <iarkeus@gmail.com>
2013-12-08 15:52:07 -0800

    Correct date points.

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -23,8 +23,10 @@ class Schedule < ActiveRecord::Base
 		points = []
 		time = Time.now
 		if schedule_type == "daily"
-			NUM_DAILY_POINTS.times do
-				points << { value: TimeUtil::day_number(time), display: time.mday.ordinalize }
+			limit = 8
+			time += 1.day until daily_days.include?(time.strftime("%A")) || (limit -= 1) <= 0
+			until points.length >= NUM_DAILY_POINTS do
+				points << { value: TimeUtil::day_number(time), display: time.mday.ordinalize, future: time.future? } if daily_days.include?(time.strftime("%A"))
 				time -= 1.day
 			end
 		elsif schedule_type == "weekly"
@@ -32,15 +34,18 @@ class Schedule < ActiveRecord::Base
 			time -= 1.day while time.wday != weekly_start && (limit -= 1) > 0
 			raise "Could not find week start" unless limit > 0
 			NUM_WEEKLY_POINTS.times do
-				ntime = time - 7.days
-				points << { value: TimeUtil::week_number(time), display: "#{ntime.mday.ordinalize} - #{time.mday.ordinalize}" }
-				time = ntime
+				points << { value: TimeUtil::week_number(time), display: "#{time.mday.ordinalize} - #{(time + 6.days).mday.ordinalize}" }
+				time -= 7.days
 			end
 		end
 		points
 	end
 	
 	def as_json(options = {})
-		super(methods: [:points])
+		super(methods: [:points], include: {
+			schedule_tasks: {
+				except: [:created_at, :updated_at]
+			}
+		}, except: [:created_at, :updated_at])
 	end
 end
