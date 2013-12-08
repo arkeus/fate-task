ce0ea2286c9baa487df76ea2db937c3436504412
Lee Miller <iarkeus@gmail.com>
2013-12-08 15:52:21 -0800

    Tasks.

--- app/models/schedule_task.rb
+++ app/models/schedule_task.rb
@@ -1,3 +1,19 @@
 class ScheduleTask < ActiveRecord::Base
 	belongs_to :schedule
+	before_save :before_save
+	after_initialize :after_initialize
+	validates :name, presence: true
+	
+	def before_save
+		self.data = nil
+	end
+	
+	def after_initialize
+		self.data = nil
+	end
+	
+	def as_json(options = {})
+		puts "\n\n\n\nASKLJDALSKJDAKLSJD\n\n\n\n"
+		super(except: [:created_at, :updated_at])
+	end
 end
