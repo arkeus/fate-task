03f18824797ecd389fa046687bfcd802bafa0bb2
Lee Miller <iarkeus@gmail.com>
2013-12-13 00:30:00 -0800

    Extract functionality and add edit function.

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,12 +1,15 @@
 class BoardsController < ApplicationController
-	def index; end
+	around_action :redirect_errors, only: [:show]
+	around_action :render_errors, only: [:edit]
+	before_action :set_board, only: [:show, :edit]
 	
-	def show
-		@board = Board.where(name: params[:board]).first
-		unless @board
-			flash[:error] = "Board with name #{params[:board]} does not exist";
-			redirect_to root_path and return
-		end
+	def index; end
+	def show; end
+	
+	def edit
+		@board.time_zone = post_params[:board][:time_zone]
+		@board.save!
+		render nothing: true, status: 200
 	end
 	
 	def create
@@ -28,7 +31,13 @@ class BoardsController < ApplicationController
 	
 	private
 	
+	def set_board
+		board_name = post_params[:board_name]
+		@board = Board.where(name: board_name).first
+		raise "Board with name #{board_name} does not exist" unless @board
+	end
+	
 	def post_params
-		params.permit(:name)
+		params.permit(:board_name, :name, board: [:name, :time_zone])
 	end
 end
