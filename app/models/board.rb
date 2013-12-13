bd4ed257a62893cbb87bb15be27d20213e280520
Lee Miller <iarkeus@gmail.com>
2013-12-13 00:28:41 -0800

    Add default time zone without needing database value.

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,9 @@
 class Board < ActiveRecord::Base
 	has_many :schedules, dependent: :destroy
 	validates :name, format: { with: /\A[a-z0-9-]+\z/, message: "Only lowercase letters, numbers, and dashes allowed in a name" }
+	after_initialize :after_initialize
+	
+	def after_initialize
+		self.time_zone = "Pacific Time (US & Canada)" unless time_zone
+	end
 end
