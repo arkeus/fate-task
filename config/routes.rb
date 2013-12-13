6c0b8a7fcc4b6c2ae15539c85ef9b08796802960
Lee Miller <iarkeus@gmail.com>
2013-12-13 00:27:16 -0800

    Change :board to :board_name, add edit route.

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Task::Application.routes.draw do
 	post "/create", to: "boards#create", as: :create_board
 	post "/load", to: "boards#load", as: :load_board
 	
-	scope ":board" do
+	scope ":board_name" do
 		root "boards#show", as: :board
+		put "/edit", to: "boards#edit", as: :edit_board
+		
 		resources :schedules, except: [:new, :edit] do
 			resources :tasks, except: [:new, :edit] do
 				member do
