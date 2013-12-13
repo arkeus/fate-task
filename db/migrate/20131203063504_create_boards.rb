6e74fe78a485a555eca8677d8b470a5b8a5a3e19
Lee Miller <iarkeus@gmail.com>
2013-12-13 00:27:30 -0800

    Add time zone to boards migration.

--- db/migrate/20131203063504_create_boards.rb
+++ db/migrate/20131203063504_create_boards.rb
@@ -2,6 +2,7 @@ class CreateBoards < ActiveRecord::Migration
   def change
     create_table :boards do |t|
 			t.string :name
+			t.string :time_zone
       t.timestamps
       
       t.index :name, unique: true
