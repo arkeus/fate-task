4ef8587378bd8fd806850c4231608693aa594475
Lee Miller <iarkeus@gmail.com>
2013-12-18 21:13:06 -0800

    No really go away.

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(:default, Rails.env)
 module Task
   class Application < Rails::Application
   	config.autoload_paths << Rails.root.join("lib")
-  	I18n.enforce_available_locales = true
+  	config.i18n.enforce_available_locales = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
